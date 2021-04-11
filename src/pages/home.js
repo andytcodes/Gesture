f959f0c4a2bbd8faae69f179a692e9e5dad1bd65
Andy <andytcodes@gmail.com>
2021-04-11 06:07:08 -0400

    added beginner path to home

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,19 +1,52 @@
 import React from 'react';
 import logo from '../images/Gesture_Logo_05.png';
+import '../styles/fonts.css';
+import { LightAccent, MainColor } from '../components/Colors';
+import BeginnerCategories from '../components/BeginnerCategory';
 
+/* 
+Demonstration purposes only. 
+Have to generate title and categories 
+according to user learning path
+*/
 const Home = () => {
   return (
     <div
       style={{
         display: 'flex',
         flexDirection: 'column',
-        justifyContent: 'center',
         alignItems: 'center',
         height: '90vh'
-      }}
-    >
-      <img src={logo} style={{width:300}} />
-      <h1>Home</h1>
+      }}>
+
+        <div style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center'
+        }}>
+          <h1 style={{
+            fontFamily: 'Montserrat',
+            fontSize: 48,
+            color: MainColor,
+            marginTop: 50, //remove if necessary
+          }}>
+            Beginners
+          </h1>
+        </div>
+        
+        <div style={{
+        display: 'flex',
+        flexDirection: 'column',
+        width: 1000
+        }}>
+          <h2 style={{
+            fontFamily: 'Montserrat',
+            color: LightAccent
+          }}>
+            Categories
+          </h2>    
+          <BeginnerCategories/>      
+        </div>
     </div>
   );
 };
