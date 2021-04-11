1b199916bdf0fbabb86698fccdf099e9109bc885
Andy <andytcodes@gmail.com>
2021-04-11 06:06:32 -0400

    added colors and beginners components

---
+++ src/components/Colors.js
@@ -0,0 +1,5 @@
+export const MainColor = "#FC9A06";
+export const LightShade = "#F3F6F4";
+export const LightAccent = "#A2A3AA";
+export const DarkShade = "#1C242F";
+export const DarkAccent = "#B36F34";
